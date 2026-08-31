65431c12bc5dcd2c1c63ea752c590f87bf01b4d0
JohnEgan <jmegan1210@yahoo.com>
2026-08-31 17:59:14 -0400

    category and segment navigation, copy shown segment to clipboard

--- components/CopyMode.jsx
+++ components/CopyMode.jsx
@@ -1,36 +1,47 @@
 import { useState, useEffect } from 'react'
-import CategoryBlock from './CategoryBlock'
 
 function CopyMode({categories, segments}) {
 
     const [catIndex, setCatIndex] = useState(0);
     const [segIndex, setSegIndex] = useState(0)
 
+    const currentCat = categories[catIndex];
+    const currentSegs = segments.filter(seg => seg.category === currentCat);
     useEffect(() => {
         function handleKey(e){
+            //TODO: s || arrowkey?
             if(e.key === 's') {
-                e.preventDefault()
+                e.preventDefault();
                 //go through this later 
-                setSelected(i => (i + 1) % items.length)
+                setSegIndex(i => (i + 1) % currentSegs.length);
             } else if(e.key === 'w') {
-                e.preventDefault()
-                setSelected(i => (i - 1 + items.length) % items.length)
+                e.preventDefault();
+                setSegIndex(i => (i - 1 + currentSegs.length) % currentSegs.length);
+            //TODO: shit+tab for backwards?
+            } else if(e.key === 'Tab'){
+                e.preventDefault();
+                setCatIndex(i => (i + 1) % categories.length);
+                //currentCat = categories[catIndex]
+                //currentSegs = segments.filter(seg => seg.category === currentCat);
+                setSegIndex(0);
+            }else if(e.key === 'Enter'){
+                e.preventDefault();
+                navigator.clipboard.writeText(currentSegs[segIndex].value)
             }
+            
         }
         window.addEventListener('keydown', handleKey);
         return () => window.removeEventListener('keydown', handleKey)
-    }, [])
+    }, [catIndex, categories, segments, segIndex])
 
     return(
         <div>
-            <h1>copy mode</h1>
-            {
-                categories.map(cat => (
-                    <h1>{cat}</h1>
-                ))
-            }
+            <h1>copy mode</h1>   
+            <h1>{currentCat}</h1>
+            <h1>{currentSegs[segIndex].value}</h1>
         </div>
     )
 }
 
 export default CopyMode
+
